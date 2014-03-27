9a1026034c75f53fa249bb612f1984e83bdbe772
Quetzacoalt91 <thomas.nabord@prestashop.com>
2014-03-27 17:16:31 +0100

    / MO statsbestcategories : min ps_versions_compliancy modified (1.5.6.1 => 1.6)

--- statsbestcategories.php
+++ statsbestcategories.php
@@ -41,7 +41,7 @@ class StatsBestCategories extends ModuleGrid
 	{
 		$this->name = 'statsbestcategories';
 		$this->tab = 'analytics_stats';
-		$this->version = 1.1;
+		$this->version = '1.2';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
@@ -81,7 +81,7 @@ class StatsBestCategories extends ModuleGrid
 
 		$this->displayName = $this->l('Best categories');
 		$this->description = $this->l('Adds a list of the best categories to the Stats dashboard.');
-		$this->ps_versions_compliancy = array('min' => '1.5.6.1', 'max' => _PS_VERSION_);
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
 	}
 
 	public function install()
