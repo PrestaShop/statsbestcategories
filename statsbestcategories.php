5bd28a7cbfabc54aaee6623638b3da42b193e384
Michel ANTOINE <michel.br.antoine@gmail.com>
2017-01-10 14:01:55 +0100

    Update minimum compliancy version
    (cherry picked from commit 3db4d12b71a130c4277d6ed2e41c67f40972ec31)

--- statsbestcategories.php
+++ statsbestcategories.php
@@ -88,7 +88,7 @@ class statsbestcategories extends ModuleGrid
 
         $this->displayName = $this->trans('Best categories', array(), 'Modules.Statsbestcategories.Admin');
         $this->description = $this->trans('Adds a list of the best categories to the Stats dashboard.', array(), 'Modules.Statsbestcategories.Admin');
-        $this->ps_versions_compliancy = array('min' => '1.7.0.0', 'max' => _PS_VERSION_);
+        $this->ps_versions_compliancy = array('min' => '1.7.1.0', 'max' => _PS_VERSION_);
     }
 
     public function install()
